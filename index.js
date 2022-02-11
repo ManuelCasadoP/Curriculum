cbd2afee2df4aadb91493cbae9d3012e74175ffb
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-11 10:11:45 +0100

    Trabajo 11/02/2022 10:11

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function main(){
 console.log("Starting......");
+const e = document.querySelector("h1");
+e.addEventListener("pointerenter",()=>{console.log("Hola")});
 
 }
 window.addEventListener("load", main);
+
+
 
 
 

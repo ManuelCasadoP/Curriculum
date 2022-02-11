bb522830f527e6ec086dc2510a82c3336bc7625f
Manuel Casado <ManuelCasado@protonmail.com>
2022-02-11 13:36:14 +0100

    Trabajo 11/02/2022 13:36

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function main(){
                 titulo.addEventListener("pointerenter", imprimeHola);
                 const elements=Array.from(document.body.querySelectorAll("*"));
                 console.log(elements);
+                for (let item of elements) {item.addEventListener("pointerenter",()=>{console.log("Pop!!")})};
+                const counters = elements.map((item)=>{return{element:item, counter:0,}});
+                console.log(counters);
 }
 
 window.addEventListener("load", main);
